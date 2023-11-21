ac1c87b191dea1eb510a14e56b9f130c4bb7ac65
YourGitHubUsername <pcatjee@gmail.com>
2023-11-21 20:28:27 +0530

    initial commit

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC-0SYlovW7yuiLCWvPX9kk2bb1kV4Hlhs',
+  authDomain: 'olxclonedemo-3fbc7.firebaseapp.com',
+  projectId: 'olxclonedemo-3fbc7',
+  storageBucket: 'olxclonedemo-3fbc7.appspot.com',
+  messagingSenderId: '260928176222',
+  appId: '1:260928176222:web:6723283b0f41743580b4cf',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
